8c68512e7370fdb7326e831e54fa20860f987d3a
fuchigami <fuchigami@adjust.ne.jp>
2017-11-02 08:32:34 +0000

    create eventListener for user login event

---
+++ app/Listeners/LoginSuccess.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Login as LoginEvent;
+
+class LoginSuccess
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  KernelBootstrapped  $event
+     * @return void
+     */
+    public function handle(LoginEvent $event)
+    {
+    }
+
+}
